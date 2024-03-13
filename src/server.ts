9aa3047b2b3b6311553d9d5836c94f3cae8fd8bd
RomilsonMonteiro <132623804+RomilsonMonteiro@users.noreply.github.com>
2024-03-13 05:08:31 +0000

    w

--- src/server.ts
+++ src/server.ts
@@ -21,14 +21,31 @@ app.get('/books', async (request, response) => {
 })
 
 app.post('/books', async (request, response) => {
-  const { description, name } = request.body
-  const book = await prismaClient.book.create({
-    data: {
-      description,
-      name,
-    },
-  })
-  return response.json(book)
+  const { description, name } = request.body;
+
+// Verificar se já existe um livro com o mesmo nome e descrição
+const existingBook = await prismaClient.book.findFirst({
+  where: {
+    name: { equals: name }, 
+    description: { equals: description }, 
+  },
+});
+
+// Se já existir um livro com os mesmos valores, retornar um erro
+if (existingBook) {
+  return response.status(400).json({ error: 'Já existe um livro com o mesmo nome e descrição.' });
+}
+
+// Se não existir, criar o novo livro
+const book = await prismaClient.book.create({
+  data: {
+    description,
+    name,
+  },
+});
+
+return response.json(book);
+
 })
 
 app.listen(port, () => console.log('Server is running on port ', port))
